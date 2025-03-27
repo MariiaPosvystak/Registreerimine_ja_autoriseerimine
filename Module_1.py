cb71107881d15bf0cb5955fa5c108e9b392cca22
opilane <opilane@HP-CZC2349HT4>
2025-03-27 15:06:47 +0200

    adv

--- Module_1.py
+++ Module_1.py
@@ -2,8 +2,8 @@
 import string
 
 # Järjendid kasutajate nimede ja paroolide hoidmiseks
-usernames = []
-passwords = []
+usernames = [12]
+passwords = [13]
 
 # Генерує випадковий пароль, який складається з 12 символів, включаючи цифри, великі та малі літери, а також спеціальні символи.
 def generate_password_auto() -> str:
@@ -39,7 +39,7 @@ def generate_password_manual(k: int) -> str:
     return sala
 
 # Перевіряє, чи відповідає пароль вимогам (містить цифри, великі та малі літери, спеціальні символи).
-def is_valid_password(password: str) -> bool:
+def is_valid_password(parool: str) -> bool:
     """ Kontrollib, kas parool vastab nõuetele.
     Paroolis peab olema vähemalt üks number, suur täht, väike täht ja erimärk.
     :param password: Parool
@@ -47,14 +47,14 @@ def is_valid_password(password: str) -> bool:
     :return: Kas parool vastab nõuetele
     :rtype: bool
     """
-    has_digit = any(char.isdigit() for char in password)
-    has_upper = any(char.isupper() for char in password)
-    has_lower = any(char.islower() for char in password)
-    has_special = any(char in ".,:;!_*-+()/#¤%&" for char in password)
+    has_digit = any(char.isdigit() for char in parool)
+    has_upper = any(char.isupper() for char in parool)
+    has_lower = any(char.islower() for char in parool)
+    has_special = any(char in ".,:;!_*-+()/#¤%&" for char in parool)
     return has_digit and has_upper and has_lower and has_special
 
 # Реєструє користувача, якщо ім'я користувача ще не існує і пароль відповідає вимогам.
-def register_user(username: str, password: str) -> any:
+def register_user(kasutajanimi: str, parool:str) -> any:
     """ Registreerib kasutaja.
     :param username: Kasutajanimi
     :type username: str
@@ -63,16 +63,11 @@ def register_user(username: str, password: str) -> any:
     :return: Kasutaja registreerimine õnnestus, vastav teade
     :rtype: any
     """
-    if username in usernames:
-        return False, "Username already exists."
-    if not is_valid_password(password):
-        return False, "Password does not meet the criteria."
-    usernames.append(username)
-    passwords.append(password)
-    return True, "Registration successful."
+    usernames.append(kasutajanimi)
+    passwords.append(parool)
 
 # Авторизує користувача, якщо ім'я користувача та пароль правильні.
-def authorize_user(username: str, password: str) -> any:
+def authorize_user(kasutajanimi: str, parool: str) -> any:
     """ Autoriseerib kasutaja.
     :param username: Kasutajanimi
     :type username: str
@@ -81,44 +76,9 @@ def authorize_user(username: str, password: str) -> any:
     :return: Kasutaja autoriseerimine õnnestus, vastav teade
     :rtype: any
     """
-    if username in usernames:
-        index = usernames.index(username)
-        if passwords[index] == password:
-            return True, "Authorization successful."
-    return False, "Invalid username or password."
-
-# Змінює пароль користувача, якщо старий пароль правильний і новий пароль відповідає вимогам.
-def change_password(username: str, old_password: str, new_password: str) -> any:
-    """ Muudab kasutaja parooli.
-    :param username: Kasutajanimi
-    :type username: str
-    :param old_password: Vana parool
-    :type old_password: str
-    :param new_password: Uus parool
-    :type new_password: str
-    :return: Parooli muutmine õnnestus, vastav teade
-    :rtype: any
-    """
-    if authorize_user(username, old_password)[0]:
-        if is_valid_password(new_password):
-            index = usernames.index(username)
-            passwords[index] = new_password
-            return True, "Password changed successfully."
-        else:
-            return False, "New password does not meet the criteria."
-    return False, "Authorization failed."
-
-# Відновлює пароль користувача, генеруючи новий випадковий пароль.
-def reset_password(username: str) -> any:
-    """ Taastab kasutaja parooli.
-    :param username: Kasutajanimi
-    :type username: str
-    :return: Parooli taastamine õnnestus, vastav teade
-    :rtype: any
-    """
-    if username in usernames:
-        new_password = generate_password_auto()
-        index = usernames.index(username)
-        passwords[index] = new_password
-        return True, f"New password is: {new_password}"
-    return False, "Username not found."
+    if kasutajanimi in usernames:
+        i = usernames.index(kasutajanimi)
+        v3=usernames[i] == parool
+    else:
+        v3=False
+    return v3
--- Registreerimine_ja_autoriseerimine.py
+++ Registreerimine_ja_autoriseerimine.py
@@ -1,26 +1,31 @@
+from ast import Break
 from Module_1 import *
 
 while True:
     print("Valikud:")
     print("1. Registreeri")
     print("2. Autoriseeri")
-    print("3. Muuda parooli")
-    print("4. Taasta parool")
-    print("5. Välju")
+    print("3. Välju")
     try:
         valik = int(input("Vali valik: "))
-    except:
-        print("Vale formaat!")
-    if valik == '1':
-        while True:
-            kasutajanimi = input("Sisesta kasutajanimi: ")
-            if kasutajanimi in usernames:
-                print("Kasutajanimi on juba olemas. Palun proovi uuesti.")
-                continue
-            parooli_valik = input("Genereeri parool automaatselt? (jah/ei): ")
+        if valik == 1:
+            while True:
+                kasutajanimi = str(input("Sisesta kasutajanimi: ")).lower()
+                if kasutajanimi in usernames:
+                    print("Kasutajanimi on juba olemas. Palun proovi uuesti.")
+                    continue
+                else:
+                    break 
+            parooli_valik = str(input("Genereeri parool automaatselt? (jah/ei): ")).lower()
+            while True:
+                if parooli_valik == "jah" or parooli_valik == "ei":
+                    break
+                else:
+                    print("Sisesta ainult jah või ei")
+            
             if parooli_valik.lower() == 'jah':
                 parool = generate_password_auto()
-                print(f"Genereeritud parool: {parool}")
+                print(f"Genereeritud parool: {parool}")  
             else:
                 while True:
                     parool = input("Sisesta parool: ")
@@ -28,47 +33,20 @@ while True:
                         break
                     else:
                         print("Parool ei vasta nõuetele. Palun proovi uuesti.")
-            õnnestus, sõnum = register_user(kasutajanimi, parool)
-            print(sõnum)
-            if õnnestus:
-                break
-
-    elif valik == '2':
-        while True:
-            kasutajanimi = input("Sisesta kasutajanimi: ")
-            parool = input("Sisesta parool: ")
-            õnnestus, sõnum = authorize_user(kasutajanimi, parool)
-            print(sõnum)
-            if õnnestus:
-                break
-            else:
-                print("Vale kasutajanimi või parool. Palun proovi uuesti.")
-
-    elif valik == '3':
-        while True:
-            kasutajanimi = input("Sisesta kasutajanimi: ")
-            vana_parool = input("Sisesta vana parool: ")
-            uus_parool = input("Sisesta uus parool: ")
-            õnnestus, sõnum = change_password(kasutajanimi, vana_parool, uus_parool)
-            print(sõnum)
-            if õnnestus:
-                break
-            else:
-                print("Parooli muutmine ebaõnnestus. Palun proovi uuesti.")
-
-    elif valik == '4':
-        while True:
-            kasutajanimi = input("Sisesta kasutajanimi: ")
-            õnnestus, sõnum = reset_password(kasutajanimi)
-            print(sõnum)
-            if õnnestus:
-                break
-            else:
-                print("Kasutajanime ei leitud. Palun proovi uuesti.")
-
-    elif valik == '5':
-        print("Väljumine...")
-        break
-
-    else:
-        print("Vale valik. Palun proovi uuesti.")
+            register_user(kasutajanimi, parool)
+        elif valik == 2:
+            while True:
+                kasutajanimi = str(input("Sisesta kasutajanimi: "))
+                parool = str(input("Sisesta parool: "))
+                if authorize_user(kasutajanimi, parool):
+                    print("Sa oled süsteemi sisse loginud")
+                    break
+                else:
+                    print("Parool ei sobi")
+        elif valik == 3:
+            print("Väljumine...")
+            break
+        else:
+            print("Vale valik. Palun proovi uuesti.")
+    except ValueError:
+        print("Vale formaat!")
